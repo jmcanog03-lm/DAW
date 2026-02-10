612b91d25dae97b1af94f3b5e9d9cf82be5d7c88
jmcanog03 <jmcanog03@educarex.es>
2026-02-10 14:11:26 +0100

    v1.2

--- Actividad_5_Jose_Manuel_Cano/src/Basico.java
+++ Actividad_5_Jose_Manuel_Cano/src/Basico.java
@@ -1,21 +1,52 @@
 public class Basico extends Subscripcion{
 
+    private double precio;
+    private int duracion;
+    private int dispositivos_activos;
+    private boolean parar_subscripcion;
+    private double total_cobrado ;
+    private double [] historial_mensual;
+
     public Basico(){
-        super();
-        super.precio = 4.99;
-        super.nombre = "";
-        super.dispositivos =1;
-        super.duracion =0;
+        this.precio = 4.99;
+        this.duracion = 0;
+        this.dispositivos_activos = 0;
+        this.parar_subscripcion = false;
+        this.total_cobrado =0;
+        this.historial_mensual = new double[duracion];
     }
 
-  
+    public Basico(int duracion){
+        this.duracion = duracion;
+        
+    }
+
 
     @Override
-    public void controlar_dispositivos(){
-        if(super.dispositivos > 1){
-            System.out.println("bloqueate");
-
+    public boolean ControlarDispositivos(){
+        if(this.dispositivos_activos > 1){
+            this.parar_subscripcion = true;
+            return false;
+            
         }
+        this.parar_subscripcion = false;
+        return true;
+    }
+
+    @Override
+    public void CobroPlan(){
+
+        double total = this.duracion * this.precio;
+        this.total_cobrado = total;
+    }
+
+    @Override
+    public double[] HistoriaMensual(){
+        for(int i =0; i < duracion; i++){
+            historial_mensual[i] = precio;
+        }
+
+        return historial_mensual;
     }
 
 
--- Actividad_5_Jose_Manuel_Cano/src/Subscripcion.java
+++ Actividad_5_Jose_Manuel_Cano/src/Subscripcion.java
@@ -1,38 +1,7 @@
-public class Subscripcion {
-    protected int duracion;
-    protected String nombre;
-    protected double precio;
-    protected int dispositivos;
-    protected int descuento;
+abstract class Subscripcion {
 
-
-    public Subscripcion(){
-        this.duracion = 0;
-        this.nombre = "";
-        this.precio = 0;
-        this.dispositivos =0;
-    }
-
-    public Subscripcion ( int duracion, String nombre, int precio, int dispositivos){
-        this.duracion = duracion;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.dispositivos = dispositivos;
-    }
-
-
-
-    public void controlar_dispositivos(){
-       
-    }
-
-  
-
-
-
-
-
+    abstract boolean ControlarDispositivos();
+    abstract void CobroPlan();
+    abstract double[] HistoriaMensual();
 
 }
-
-
