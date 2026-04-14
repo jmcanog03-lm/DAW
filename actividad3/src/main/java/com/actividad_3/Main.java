a613a6f5a082e379e3646e1382e830c6f3afb370
jmcanog03-lm <jmcanog03@iesalbarregas.es>
2026-04-14 12:24:14 +0200

    v1.2

--- actividad3/src/main/java/com/actividad_3/Main.java
+++ actividad3/src/main/java/com/actividad_3/Main.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.Writer;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
@@ -12,10 +13,11 @@ public class Main {
     static String path = "src/main/java/com/actividad_3/chat-toxico-lol.txt";
     static String path2 = "src/main/java/com/actividad_3/baneos.txt";
     static HashSet<String> palabrasBaneadas = new HashSet<>();
+    static HashMap <String,Integer> conteojugadores = new HashMap<>();
+    static HashSet <String> jugadoresBaneados = new HashSet<>();
     static Scanner sc = new Scanner(System.in);
-
+    static int contador;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         palabrasBaneadas.add("pelotudo");
         palabrasBaneadas.add("boludo");
         palabrasBaneadas.add("muerte");
@@ -33,19 +35,46 @@ public class Main {
             String linea;
 
             while ((linea = br.readLine()) != null) {
-                linea = linea.replace(",", " , ").replace(".", " . ").replace("!", "").replace("?", "")
-                .replace(";", "").replace(":", "");
+                //linea = linea.replace(",", " , ").replace("." , " . ").replace("!", "").replace("?", "")
+                //.replace(";", "").replace(":", "");
 
                 //Quitar espacios generados por el replace y con el split donde haya un espacio divide
-                String palabras[] = linea.trim().split(" ");
+                String palabras[] = linea.trim().split("\\s+");
+                
                 String lineanueva = "";
+                String jugador = "";
+
+                if(palabras.length >1){
+                    jugador = palabras[1];
+                }
+
 
                 for (int i = 0; i < palabras.length; i++) {
+                    String original = palabras[i];
+                    String palabraLimpia = original.toLowerCase().replaceAll("[.,!?:;]", "");
+                    
 
-                    if (palabrasBaneadas.contains(palabras[i])) {
+                    if (palabrasBaneadas.contains(palabraLimpia)) {
                         // String palabraCensurada = ch
                         String asteriscos = "";
                         // String.valueOf(ch);
+                   
+                        
+
+                        if(conteojugadores.containsKey(jugador)){
+                            conteojugadores.put(jugador, conteojugadores.get(jugador) +1);
+                        }else{
+                            conteojugadores.put(jugador, 1);
+                        }
+
+                        if(conteojugadores.get(jugador)  > 5){
+
+                            if(!jugadoresBaneados.contains(jugador)){
+                                jugadoresBaneados.add(jugador);
+                                System.out.println("Jugador: " + jugador + " ha sido baneado");
+                            }
+
+                        }
 
                         // https://github.com/Serra-albarregas/Java2526/blob/main/src/UT7/Ejemplos/EjLeerFichero.java
                         char primeraletra = palabras[i].charAt(0);
@@ -55,10 +84,10 @@ public class Main {
                             asteriscos += "*";
                         }
 
-                        lineanueva += primeraletra + asteriscos + ultimaletra;
+                        lineanueva += " " + primeraletra + asteriscos + ultimaletra;
 
                     } else {
-                        lineanueva += palabras[i] + " ";
+                        lineanueva += original + " ";
                     }
 
                 }
